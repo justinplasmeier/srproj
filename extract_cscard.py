f85e8f86ac3707134c40fd93ed17d90d11f48f4f
Justin Plasmeier <jgp45@case.edu>
2017-04-25 01:20:56 -0400

    adds a column for coordinate location using geo point when lat and long are found.

--- extract_cscard.py
+++ extract_cscard.py
@@ -7,42 +7,51 @@ data_files = os.listdir(DATA_PATH)
 data_paths = [os.path.join(DATA_PATH, x) for x in data_files]
 
 def guess_data_type(value):
-	pattern = re.compile("^\-*\d*\.?\d*$")
-	to_compare = unicode(value)
-	if to_compare == unicode("PrivacySuppressed") or to_compare == unicode("NULL"):
-		return "DECIMAL"
-	elif re.match(pattern, to_compare):
-		return "DECIMAL"
-	return "VARCHAR(100)"
+   pattern = re.compile("^\-*\d*\.?\d*$")
+   to_compare = unicode(value)
+   if to_compare == unicode("PrivacySuppressed") or to_compare == unicode("NULL"):
+   	return "DECIMAL"
+   elif re.match(pattern, to_compare):
+   	return "DECIMAL"
+   return "VARCHAR(100)"
 
 def extract_csv(filename):
 
-	with open(filename, 'rb') as csvfile:
-		spamreader = csv.reader(csvfile, delimiter=',')
-		column_name = list()
-		data_type = list()
-		for val in spamreader.next():
-			column_name.append(val)
-	#zip needs to be delt with differently (as varchar(10))
-	#NULL is numeric, so is PrivacySuppressed 
-		i = 0
-		for val in spamreader.next():
-			# architecture built to be able to modify this stage as needed.
-			# This is where we would add other features such as unique constraints, ect.
-			if(column_name[i] == "ZIP"):
-				data_type.append(unicode("VARCHAR(10)"))
-			else:
-				data_type.append(guess_data_type(val))
-			i+=1
+    with open(filename, 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+        column_name = list()
+        data_type = list()
+        i = 0
+        latitude = None
+        longitude = None
+        for val in spamreader.next():
+            column_name.append(val)
+            # architecture built to be able to modify this stage as needed.
+            # This is where we would add other features such as unique constraints, ect.
+            if column_name[i] == "ZIP":
+                data_type.append(unicode("VARCHAR(10)"))
+            else:
+      			data_type.append(guess_data_type(val))
+            if column_name[i] == "LATITUDE":
+                latitude = val
+            if column_name[i] == "LONGITUDE":
+                longitude = val    
+       
+            if latitude is not None and longitude is not None:
+                data_type.append(unicode("geography(POINT,4326)"))
+                column_name.append("COORDINATE")
+                latitude = None
+                longitude = None
+            i+=1
 
-	f = open('schema_cscard.sql', 'w')
-	f.truncate()
-	f.write("CREATE TABLE college_scorecard ( \n")
-	for i in xrange(len(column_name)):
-		feature = "  %s %s,\n"%(column_name[i],data_type[i])
-		f.write(feature)
-	f.write("CONSTRAINT persons_pkey PRIMARY KEY (UNITID) )")
-	f.close()
+    f = open('schema_cscard.sql', 'w')
+    f.truncate()
+    f.write("CREATE TABLE college_scorecard ( \n")
+    for i in xrange(len(column_name)):
+        feature = "  %s %s,\n"%(column_name[i],data_type[i])
+        f.write(feature)
+    f.write("CONSTRAINT persons_pkey PRIMARY KEY (UNITID) )")
+    f.close()
 
 
 if __name__ == "__main__":
