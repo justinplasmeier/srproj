9e80cad19253213c3e4795e1240d55b90c09de50
Justin Plasmeier <jgp45@case.edu>
2017-04-28 10:19:36 -0400

    refactor in preparation for transform schema implementation

--- extract_cscard.py
+++ extract_cscard.py
@@ -6,52 +6,52 @@ DATA_PATH = '/home/jgp/srproj/data'
 data_files = os.listdir(DATA_PATH)
 data_paths = [os.path.join(DATA_PATH, x) for x in data_files]
 
-def guess_data_type(value):
-   pattern = re.compile("^\-*\d*\.?\d*$")
-   to_compare = unicode(value)
-   if to_compare == unicode("PrivacySuppressed") or to_compare == unicode("NULL"):
-   	return "DECIMAL"
-   elif re.match(pattern, to_compare):
-   	return "DECIMAL"
-   return "VARCHAR(100)"
+def guess_data_type(header, value):
+    """
+    Takes a value and guesses its type.
+    """
+    if header == 'ZIP':
+        return unicode("VARCHAR(10)")
+
+    pattern = re.compile("^\-*\d*\.?\d*$")
+    to_compare = unicode(value)
+    if to_compare == unicode("PrivacySuppressed") or to_compare == unicode("NULL"):
+        return "DECIMAL"
+    elif re.match(pattern, to_compare):
+        return "DECIMAL"
+    return "VARCHAR(100)"
 
 def extract_csv(filename):
+    """
+    Read the column headers and their inferred value into a dict.
+    """
+    schema_dict = {}
 
     with open(filename, 'rb') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',')
-        column_name = list()
-        data_type = list()
-        i = 0
-        latitude = None
-        longitude = None
-        for val in spamreader.next():
-            column_name.append(val)
-            # architecture built to be able to modify this stage as needed.
-            # This is where we would add other features such as unique constraints, ect.
-            if column_name[i] == "ZIP":
-                data_type.append(unicode("VARCHAR(10)"))
-            else:
-      			data_type.append(guess_data_type(val))
-            if column_name[i] == "LATITUDE":
-                latitude = val
-            if column_name[i] == "LONGITUDE":
-                longitude = val    
-       
-            if latitude is not None and longitude is not None:
-                data_type.append(unicode("geography(POINT,4326)"))
-                column_name.append("COORDINATE")
-                latitude = None
-                longitude = None
-            i+=1
+        column_names = []
+        data_type = [] 
+
+        headers = spamreader.next()
+        value_row = spamreader.next()
+
+        for col_name, value in zip(headers, value_row):
+            schema_dict[col_name] = guess_data_type(col_name, value)
 
     f = open('schema_cscard.sql', 'w')
     f.truncate()
     f.write("CREATE TABLE college_scorecard ( \n")
-    for i in xrange(len(column_name)):
-        feature = "  %s %s,\n"%(column_name[i],data_type[i])
+    for key in schema_dict.keys():
+        feature = "  %s %s,\n"%(key,schema_dict[key])
         f.write(feature)
     f.write("CONSTRAINT persons_pkey PRIMARY KEY (UNITID) )")
     f.close()
+
+def apply_xform():
+    """
+    Appends the extra columns from the transform step to the schema.
+    """
+    pass
 
 
 if __name__ == "__main__":
