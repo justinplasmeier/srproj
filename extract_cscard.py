30ccead9e326ffaad51d3ace0a5fe1141e0bc520
root <root@geocivics.case.edu>
2017-04-28 11:53:39 -0400

    modify schema name for each new update

--- extract_cscard.py
+++ extract_cscard.py
@@ -6,6 +6,21 @@ DATA_PATH = '/home/jgp/srproj/data'
 data_files = os.listdir(DATA_PATH)
 data_paths = [os.path.join(DATA_PATH, x) for x in data_files]
 LOAD_PATH = '/home/jgp/load/data'
+COUNT = 0
+
+def get_count():
+    with open('count.txt') as f:
+        COUNT = int(f.read())
+        print COUNT
+    return COUNT
+
+def set_count(count):
+    f = open('count.txt', 'r+')
+    f.truncate()
+    value = int(count)+1
+    text = str(value)
+    f.write(text)
+    f.close()
 
 def guess_data_type(header, value):
     """
@@ -43,7 +58,7 @@ def extract_csv(filename):
 def dump_to_csv(filename, schema_dict):
     f = open(filename, 'w')
     f.truncate()
-    f.write("CREATE TABLE college_scorecard ( \n")
+    f.write("CREATE TABLE college_scorecard_"+str(COUNT)+ " ( \n")
     for key in schema_dict.keys():
         feature = "  %s %s,\n"%(key,schema_dict[key])
         f.write(feature)
@@ -60,9 +75,11 @@ def apply_xform():
 
 
 if __name__ == "__main__":
+    COUNT = get_count()
     for data_file in data_files:
         data_path = os.path.join(DATA_PATH, data_file)
         load_path = os.path.join(LOAD_PATH, data_file)
         schema_dict = extract_csv(data_path)
         f = dump_to_csv(load_path[:-4] + 'schema.sql', schema_dict)
         print("wrote to:", f)
+    set_count(COUNT)
